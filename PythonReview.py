eb7e400233debd2afaa21d1481e7820b9073b0d9
ziv17-meet <ziv17@meet.mit.edu>
2015-11-04 17:54:58 +0200

    any comment about th

---
+++ PythonReview.py
@@ -0,0 +1,30 @@
+print("you are the best")
+nu1=50
+nu2=2.5
+print(nu1+nu2)
+print("this is the first number %i this is the second number %.1f" %(nu1,nu2))
+print("ziv")
+number1 = 2
+print(number1)
+number2=number1/2
+print(number2)
+tot = 0
+start_list = [4,7,9]
+for item in start_list:
+	print (item)
+	item2 = item * 2
+	print(item2)
+	tot = tot + item
+
+print (tot)
+	
+import turtle
+turtle.penup()
+turtle.goto(100,100)
+turtle.pendown()
+turtle.goto(200,100)
+turtle.goto(200,0)
+turtle.goto(100,0)
+turtle.goto(100,100)
+turtle.circle(55)
+turtle.mainloop()
